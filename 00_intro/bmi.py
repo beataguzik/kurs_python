c2b6da352c5188ec3b57875e3c88c131bb827d01
beataguzik <betty_cz@poczta.fm>
2023-04-18 17:36:13 +0200

    first commit

---
+++ 00_intro/bmi.py
@@ -0,0 +1,9 @@
+
+
+w = input("Podaj wagę ->")
+h = input("Podaj wzrost ->")
+print("Your BMI is:", w / (h ** 2))
+w = float(input("Podaj wagę ->"))
+h = float(input("Podaj wzrost ->"))
+bmi = w / (h ** 2)
+print("Your BMI is:", round(bmi, 2))
---
+++ 00_intro/lerning.py
@@ -0,0 +1,3 @@
+nauka=75
+moj_czas_w_tyg=6
+print ('do nauki potrzebuje', nauka//moj_czas_w_tyg)
