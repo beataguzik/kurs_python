771cbb1842d52bcdab3c5e1a2587817a2afca7d2
beataguzik <131194720+beataguzik@users.noreply.github.com>
2023-04-25 21:09:53 +0200

    Add files via upload

---
+++ 00_intro/test.py
@@ -0,0 +1,14 @@
+s="niebo jest niebieskie"
+
+print(s[4:8])
+
+print("domek jest cieply i wygodny"[1:-5:10])
+
+txt="abrakadabra"
+n=txt.count("a")
+print(n)
+
+
+a="ala ma kota"
+s=a.split(' ')
+print(s)
